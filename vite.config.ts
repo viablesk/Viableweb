03f9cae373329b3f8ad1ab0728ceefddd24e0032
Marian Hello <m.hello@gmail.com>
2026-01-13 20:29:50 +0100

    fix: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/Viableweb/',
+    base: '/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
